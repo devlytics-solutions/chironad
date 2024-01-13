4fb32bd38e38672627dfdc346f045e22da978a35
krys2fa <christopher.k.amanor@gmail.com>
2024-01-13 03:49:41 +0000

    Setup folder structure and layout of the app

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
+import './ui/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Lama Dev Next.js Admin Dashboard',
-  description: 'Next.js Tutorial',
+  title: 'Chironaid Admin Dashboard',
+  description: 'Medical Administration',
 }
 
 export default function RootLayout({ children }) {
