fb3cb97ba97a3d4bf8d96c46f240fdd030d674d1
krys2fa <christopher.k.amanor@gmail.com>
2024-01-13 07:33:53 +0000

    Complete design for sidebar

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,6 +1,77 @@
+import styles from "./sidebar.module.css"
+import Menulink from "./menulink/menulink"
+import Image from "next/image";
+import {MdDashboard, MdSupervisedUserCircle} from "react-icons/md"
+
+const menuItems = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />,
+      },
+    ],
+  },
+  {
+    title: "Analytics",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />,
+      },
+    ],
+  },
+  {
+    title: "User",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />,
+      },
+    ],
+  },
+]
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image src="/noavatar.png" alt="" width="50" height="50" />
+        <div className={styles.userDetail}>
+          <span className={styles.username}>John Doe</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
+      </div>
+      <ul className={styles.list}>
+        {menuItems.map(cat => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map(item => ( 
+              <Menulink item={item} key={item.title} />
+            ))} 
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
